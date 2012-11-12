c351e67d94b19ff35f89ff846e13e048d26016cb
Michael Alves Lobo <root@kairel-Satellite-A200.(none)>
2012-11-12 21:26:16 +0100

    add search result text

--- template/public.php
+++ template/public.php
@@ -11,15 +11,21 @@ echo '<div id = "blogContent">';
 $lucene = new lucene();
 $html = '';
 
-
+$nb_result = 0;
 foreach($lucene->search(strtoupper(htmlentities(utf8_decode(urldecode($_GET['searchValue']))))) as $item){
     $html .= '<div class = "blogEntry">';
     $html .= '<h2><a href = "'.$item['href'].'" >'.$item['title'].'</a></h2>';
     $html .= '<div class = "blogEntryContent">'.strip_tags(substr($item['content'],0,200)).' ...</div>';
     $html .= '</div>';
+    $nb_result++;
 }
-
-echo $html;
+if ($nb_result > 0){
+	$result_html = "<p id = 'search_text'>".$nb_result." r&#233;sultat(s) &#225; votre recherche</p>";
+}
+else {
+	$result_html = "<p id = 'search_text'>Aucun r&#233;sultat.</p>";
+}
+echo $result_html.' '.$html;
 echo '</div>';
 ?>
 <?php include_once(ROOT.'theme/'.$coreConf['theme'].'/footer.php') ?>
